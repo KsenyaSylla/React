2545f1b3c7d4e9f3e6d28f4652b98ac351342075
Sylla Ksenia <natoire@yandex.ru>
2021-11-03 22:02:17 +0300

    + ListItemText

--- hw1/src/components/Chat.jsx
+++ hw1/src/components/Chat.jsx
@@ -1,16 +1,15 @@
 import React from "react";
-import { ListItem, Paper, Button } from "@material-ui/core";
+import { ListItem, ListItemText, Paper, Button } from "@material-ui/core";
 import { HighlightOff, Launch } from "@material-ui/icons";
 import { makeStyles } from '@material-ui/core/styles';
 import propTypes from "prop-types";
 
 const useStyles = makeStyles((theme) => ({
-    wrapper: {
-        display: "flex",
-    },
     paper: {
         padding: theme.spacing(1),
         flexGrow: 1,
+        display: "flex",
+        alignItems: "center"
     },
     deleteButton: {
         color: "red",
@@ -22,12 +21,12 @@ export const Chat = (props) => {
     const classes = useStyles();
     let item = props.item;
     return (
-        <ListItem className={classes.wrapper}>
+        <ListItem >
             <Paper className={classes.paper}>
                 <Button>
                     <HighlightOff className={classes.deleteButton} />
                 </Button>
-                {item.author}
+                <ListItemText>{item.author}</ListItemText>
                 <Button><Launch /></Button>
             </Paper>
         </ListItem>);
